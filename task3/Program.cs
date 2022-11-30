b7b5765f750243f47e0a884fbc23a66cd5254dc2
Biramax <biramax@biramax>
2022-11-30 14:24:27 +0300

    Add program on task 3

--- task3/Program.cs
+++ task3/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+
+Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+4 -> да
+-3 -> нет
+7 -> нет
+
+*/
+
+Console.Write($"Введите число: ");
+string input = Console.ReadLine() ?? "";
+
+if (int.TryParse(input, out int num)) 
+{
+    string output = $"Число {num} ";
+    
+    if (num % 2 == 0) output += "чётное";
+    else              output += "нечётное";
+    
+    Console.WriteLine(output);
+}
+else
+    Console.WriteLine("Вы ничего не ввели или ввели не число. Выход из программы.");
+    
